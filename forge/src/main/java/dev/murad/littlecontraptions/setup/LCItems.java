9a25c9cb2d0bcad5aa1aa76e84b40aa5f26536bf
EDToaster <howard3061@gmail.com>
2022-03-14 00:16:13 -0700

    Add lombok, Refactor registration, Update model

--- forge/src/main/java/dev/murad/littlecontraptions/setup/LCItems.java
+++ forge/src/main/java/dev/murad/littlecontraptions/setup/LCItems.java
@@ -7,8 +7,14 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class LCItems {
 
-    public static final RegistryObject<Item> CONTRAPTION_BARGE_ITEM = Registration.ITEMS.register("contraption_barge",
-            () -> new ContraptionBargeItem(new Item.Properties().tab(CreativeModeTab.TAB_TRANSPORTATION)));
+    public static final Registration registration = new Registration();
+    static {
+        registration.setCurrentTab(CreativeModeTab.TAB_TRANSPORTATION);
+    }
+
+    public static final RegistryObject<Item> CONTRAPTION_BARGE_ITEM =
+            registration.item("contraption_barge", ContraptionBargeItem::new)
+            .register();
 
     public static void register () {
     }
