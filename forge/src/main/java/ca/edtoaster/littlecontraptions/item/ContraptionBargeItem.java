1f65ddc71a52fbdc226cfd31d903c527ba7175f0
UdoMorgenstern <webmaster@udo-morgenstern.com>
2022-09-27 20:23:48 +0200

    not so horrible after all

--- forge/src/main/java/ca/edtoaster/littlecontraptions/item/ContraptionBargeItem.java
+++ forge/src/main/java/ca/edtoaster/littlecontraptions/item/ContraptionBargeItem.java
@@ -1,15 +1,34 @@
 package ca.edtoaster.littlecontraptions.item;
 
 import ca.edtoaster.littlecontraptions.entity.ContraptionBargeEntity;
+
+import com.mojang.datafixers.util.Function4;
+
 import dev.murad.shipping.item.VesselItem;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 
+
+import java.util.Optional;
+
+
 // TODO: Refactor Item in LL to reduce class bloat
 public class ContraptionBargeItem  extends VesselItem {
-    public ContraptionBargeItem(Properties p_i48526_2_) {
+
+    private final Function4<Level, Double, Double, Double, Entity> addEntity;
+    private Optional<String> tooltipLocation = Optional.empty();
+
+
+    public ContraptionBargeItem(Properties p_i48526_2_,Function4<Level, Double, Double, Double, Entity> addEntity, String tooltip) {
         super(p_i48526_2_);
+        this.addEntity = addEntity;
+        this.tooltipLocation = Optional.of(tooltip);
+    }
+
+    public ContraptionBargeItem(Properties p_i48526_2_, Function4<Level, Double, Double, Double, Entity> addEntity) {
+        super( p_i48526_2_);
+        this.addEntity = addEntity;
     }
 
     @Override
